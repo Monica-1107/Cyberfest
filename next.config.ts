4fc7ea80f792522a31b37cb6f59ba11b6b4435d6
Monica-1107 <vatticharlamonica@gmail.com>
2026-02-07 14:00:56 +0530

    Fix Netlify deployment: Remove static export, add Next.js plugin

--- next.config.ts
+++ next.config.ts
@@ -8,14 +8,24 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
